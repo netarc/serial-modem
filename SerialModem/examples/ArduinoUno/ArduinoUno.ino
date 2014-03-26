75fe5d3a546fb08b5c6d20d3f14c173f7c325b77
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2014-03-25 18:21:07 -0700

    update examples

--- SerialModem/examples/ArduinoUno/ArduinoUno.ino
+++ SerialModem/examples/ArduinoUno/ArduinoUno.ino
@@ -1,43 +1,27 @@
-#include "jsonlite.h"
-#include "M2XStreamClient.h"
 #include "SerialModem.h"
 
-SerialModemClient client;
-SerialModemGPS    gps;
-
-char m2xFeedId[] = "...";    // Feed you want to post to
-char m2xKey[] = "...";       // Your M2X access key
-M2XStreamClient m2xClient(&client, m2xKey);
-
 void setup() {
-  delay(5000);
-
   Serial.begin(115200);
   while (!Serial);
-  Serial.println("setup");
 
-  SerialModem.setHardwareInterface(Modem_SIM5218);
+  SerialModem.setHardwareInterface(INTERFACE_SIM5218);
   SerialModem.begin(&Serial, 115200);
   while (!SerialModem.ready()) {
-    Serial.println("waiting for modem ready");
-    delay(2000);
+    Serial.println("waiting for modem ready..");
+    delay(100);
   }
 
-  while (SerialModem.getNetworkStatus() < Modem::ROAMING) {
+  while (SerialModem.getNetworkStatus() < NETWORK_STATUS_ROAMING) {
     Serial.println("waiting for network registration");
     delay(2000);
   }
 
-  while (!SerialModem.setAPN("m2m.com.attz")) {
+  while (!SerialModem.setAPN(PROGMEM_STR("m2m.com.attz"))) {
     Serial.println("setting APN");
     delay(2000);
   }
 }
 
 void loop() {
-  int result = m2xClient.post(m2xFeedId, "temperature", 12.1f);
-  Serial.print("M2x client response code: ");
-  Serial.println(result);
 
-  delay(10000);
 }
