42c59d883d63b0ca9b607e4ecaf57fb3d11bc649
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2014-05-01 16:42:05 -0700

    add LP Tiva example

---
+++ SerialModem/examples/LaunchPadTiva/LaunchPadTiva.ino
@@ -0,0 +1,26 @@
+#include "SerialModem.h"
+
+void setup() {
+  Serial.begin(115200);
+
+  SerialModem.setDriver(DRIVER_SIM5218);
+  SerialModem.setSerial(&Serial1, 115200);
+  while (!SerialModem.ready()) {
+    Serial.println("waiting for modem ready..");
+    delay(100);
+  }
+
+  while (SerialModem.getNetworkStatus() < NETWORK_STATUS_ROAMING) {
+    Serial.println("waiting for network registration");
+    delay(2000);
+  }
+
+  while (!SerialModem.setAPN("m2m.com.attz")) {
+    Serial.println("setting APN");
+    delay(2000);
+  }
+}
+
+void loop() {
+
+}
